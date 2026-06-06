53fd183755379f2e57f2276aa069761a4c510066
Eden Oren <edenoren@gmail.com>
2026-06-06 11:16:07 +0300

    feat(i18n): wire TranslateService with APP_INITIALIZER and complete i18n coverage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { TranslateService } from './shared/services/translate.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (translate: TranslateService) => () => translate.load(),
+      deps: [TranslateService],
+      multi: true,
+    },
   ]
 };
